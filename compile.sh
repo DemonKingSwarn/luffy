e26cb813735d6bd7d9ad91495ccf876a2174641a
DemonKingSwarn <rockingswarn@gmail.com>
2026-01-25 14:37:38 +0530

    chore: compile script updated

--- compile.sh
+++ compile.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
 
 platform=$(uname -s)
+arch=$(uname -m)
 
 if [[ "$platform" == "Linux" ]]; then
-  go build -ldflags="-s -w" -o luffy.amd64
-  upx --best --lzma luffy.amd64
+  if [[ "$arch" == "aarch64" ]]; then
+    go build -ldflags="-s -w" -o luffy.aarch64
+    upx --best --lzma luffy.aarch64
+  else
+    go build -ldflags="-s -w" -o luffy.amd64
+    upx --best --lzma luffy.amd64
+  fi
 else
   go build -o luffy-macos.aarch64
 fi
