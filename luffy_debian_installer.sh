5392abbcb5c59579d25e82e41649067156a5a416
DemonKingSwarn <rockingswarn@gmail.com>
2026-01-25 23:34:10 +0530

    chore: debian installer update

--- luffy_debian_installer.sh
+++ luffy_debian_installer.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-LUFFY_VERSION="1.0.11"
+LUFFY_VERSION="1.0.12"
 LUFFY_URL="https://github.com/demonkingswarn/luffy"
-LUFFY_BINARY="${LUFFY_URL}/releases/download/v${LUFFY_VERSION}/luffy.amd64"
+LUFFY_BINARY="${LUFFY_URL}/releases/download/v${LUFFY_VERSION}/luffy-linux-amd64"
 DEPENDS="fzf chafa mpv libsixel-bin yt-dlp"
 
 sudo apt install -y $DEPENDS
